c94c167c1067eb3fe91f3ab87fc264d16a18c181
misti <idlkz2019@gmail.com>
2024-02-27 21:55:09 +0600

    123

--- resources/views/freedom/freedom-info.blade.php
+++ resources/views/freedom/freedom-info.blade.php
@@ -11,6 +11,131 @@
                 </div>
 
             </div>
+            @if($data)
+                <div class="col-12 py-3 px-2">
+                    <ul class="list-group my-3">
+                        <li class="list-group-item d-flex align-items-center">
+                            Статус:
+                            @if($data["result"] == "REJECTED")
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    Отказано
+                                </span>
+                            @endif
+                            @if($data["result"] == "APPROVED")
+                                <span class="badge badge-success badge-pill ml-2">
+                                    Принято в обработку
+                                </span>
+                            @endif
+                            @if($data["result"] == "ISSUED")
+                                <span class="badge badge-success badge-pill ml-2">
+                                    Заявка завершена
+                                </span>
+                            @endif
+                        </li>
+                        @if($data["uuid"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Уникальный идентификатор:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["uuid"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["alternative_reason"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Причина:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["alternative_reason"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["alternative_sum"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Альтернативная сумма:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["alternative_sum"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["approved_params"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Месяцы:
+                                <span class="badge badge-info badge-pill ml-2">
+                                    {{$data["approved_params"]["period"]}}
+                                </span>
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                Сумма:
+                                <span class="badge badge-info badge-pill ml-2">
+                                    {{$data["approved_params"]["principal"]}} KZT
+                                </span>
+                            </li>
+
+                            <li class="list-group-item d-flex align-items-center">
+                                Месячная оплата:
+                                <span class="badge badge-info badge-pill ml-2">
+                                    {{$data["approved_params"]["monthly_payment"]}} KZT
+                                </span>
+                            </li>
+                        @endif
+                        @if($data["product"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Тип:
+                                @if($data["product"] == "REMASTERKZ" || $data["product"] == "REMASTERKZ_24")
+                                    <span class="badge badge-info badge-pill ml-2">
+                                        Рассрочка
+                                    </span>
+                                @endif
+                                @if($data["product"] == "REMASTERKZ_CR" || $data["product"] == "REMASTERKZ_CR48")
+                                    <span class="badge badge-info badge-pill ml-2">
+                                        Кредит
+                                    </span>
+                                @endif
+                            </li>
+                        @endif
+                        @if($data["iin"])
+                            <li class="list-group-item d-flex align-items-center">
+                                ИИН:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["iin"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["mobile_phone"])
+                            <li class="list-group-item d-flex align-items-center">
+                                ИИН:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["mobile_phone"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["first_name"])
+                            <li class="list-group-item d-flex align-items-center">
+                                ИМЯ:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["first_name"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["last_name"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Фамилия:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["last_name"]}}
+                            </span>
+                            </li>
+                        @endif
+                        @if($data["middle_name"])
+                            <li class="list-group-item d-flex align-items-center">
+                                Отчество:
+                                <span class="badge badge-danger badge-pill ml-2">
+                                    {{$data["middle_name"]}}
+                            </span>
+                            </li>
+                        @endif
+                    </ul>
+                    <hr/>
+                </div>
+            @endif
 
 
         </div>
